d7e49f2ae34c70b9e2301260a7bf185e78d3ce7c
Kelvin_laptop <kevin_lu@maio.co>
2023-10-11 21:12:59 +0800

    Upload practice08.

---
+++ Practice08_Event.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.18;
+contract PiggyBank{
+    address public owner;
+    event Create(address owner, uint value);
+    event Receive(address sender, uint value);
+    event Withdraw(address owner, uint value);
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Withdraw Failed,Could withdraw by onwer.");
+        _;
+    }
+    constructor() payable {
+        owner = msg.sender; // It only runs during deployment,so sender is who deploys it. 
+        emit Create(owner, msg.value);
+    }
+    receive() external payable {
+        emit Receive(msg.sender, msg.value);
+    }
+    function withdraw() onlyOwner external{
+        address payable receiver = payable(msg.sender);
+        uint value = address(this).balance;
+        receiver.transfer(value);
+        emit Withdraw(receiver, value);
+    }
+}
+
+// declare : event [name](...);
+// call : emit [name](...);
+
+// indexed : An indexed Event will be placed in Topics column.Topics include 4 bytes32 columns.
+//             The first column puts [keccak(name,.....)].The other column can contain anything and
+//             won't be encoded.
+
+// Non-indexed
