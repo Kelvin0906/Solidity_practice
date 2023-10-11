336883ff439f37861994fb1ec9fb829d5c838c67
Kelvin_laptop <kevin_lu@maio.co>
2023-10-11 20:38:29 +0800

    upload practice07

---
+++ Practice07_ControlStructure.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.18;
+contract class{
+}
+
+// if(criteria) {"If true"} else if(criteria) {} else{"if false"}
+
+// for( declare ; criteria ; Change something after the code finishes.){}
+
+// while(criteria) {}
+
+// break
+// continue
